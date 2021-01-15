94209cbe65c20263ccc86c2a8bc9097d4eae6a17
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2021-01-15 08:51:48 -0800

    Get route added

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
+import os
 
+FLASK_DB_URI = os.environ.get("FLASK_DB_URI")
+
+client = MongoClient(FLASK_DB_URI)
+db = client.baby_temp_db
 app = Flask(__name__, static_folder='../build', static_url_path="/")
 
 @app.route("/")
 def index():
     return app.send_static_file('index.html')
+
+@app.route("/temperatures")
+def get_temps():
+    temperatures_cursor = db.find()
+    temperatures = []
+    for row in temperatures_cursor:
+        temperatures.append(row)
+    return jsonify(temperatures)
 
 if __name__ == "__main__":
     app.run()
