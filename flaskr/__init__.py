104f56a0f25686aba136738364765249d188fdd6
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2021-01-15 15:42:18 -0800

    pymongo connected to atlas

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+from datetime import datetime
 import os
 
 FLASK_DB_URI = os.environ.get("FLASK_DB_URI")
 
 client = MongoClient(FLASK_DB_URI)
-db = client.baby_temp_db
+db = client.babytempdb
 app = Flask(__name__, static_folder='../build', static_url_path="/")
 
 @app.route("/")
@@ -14,9 +15,10 @@ def index():
 
 @app.route("/temperatures")
 def get_temps():
-    temperatures_cursor = db.find()
+    temperatures_cursor = db.temperatures.find()
     temperatures = []
     for row in temperatures_cursor:
+        row.pop('_id')
         temperatures.append(row)
     return jsonify(temperatures)
 
