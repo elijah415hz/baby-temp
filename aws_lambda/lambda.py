52dad56288e712e9d7c80de88b4bcd6f0af20f20
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2021-01-24 13:39:22 -0800

    Parsing timestamp into string in this function now

--- aws_lambda/lambda.py
+++ aws_lambda/lambda.py
@@ -3,15 +3,20 @@ from pymongo import MongoClient
 import os
 DB_URI = os.environ.get("DB_URI")
 
-
 def lambda_handler(event, context):
+    #
     with MongoClient(DB_URI) as client:
         db = client.babytempdb
-        temperatures = []
+        temperatures = {
+            'labels': [],
+            'inside': [],
+            'outside': []
+            }
         for temp in db.temperatures.find():
-            temp.pop('_id')
-            temp['timestamp'] = temp['timestamp'].strftime("%d-%m-%Y")
-            temperatures.append(temp)
+            temp['timestamp'] = temp['timestamp'].strftime("%-I:%M %p")
+            temperatures['labels'].append(temp['timestamp'])
+            temperatures['inside'].append(temp['room_temp'])
+            temperatures['outside'].append(temp['outside_temp'])
     return {
         'statusCode': 200,
         'body': json.dumps(temperatures)
